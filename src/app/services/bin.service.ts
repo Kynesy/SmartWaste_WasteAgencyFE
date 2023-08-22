d29ed69d5108d67877fdb9535477a0897863b785
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-22 19:26:02 +0200

    implementato bin service
    - rimossi bin mock
    - inserite chiamate al backend

--- src/app/services/bin.service.ts
+++ src/app/services/bin.service.ts
@@ -2,152 +2,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { Bin } from '../models/bin';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BinService {
   authToken: string | null = null
-  baseUrl: string = "http://localhost:8080/api/user/";
-  
-  bins: Bin[] = [
-    {
-      id: '1',
-      lat: 40.3539,
-      long: 18.1756,
-      capacity: 100,
-      sortedWaste: 10,
-      unsortedWaste: 30,
-      alertLevel: 0
-    },
-    {
-      id: '2',
-      lat: 40.3600,
-      long: 18.1800,
-      capacity: 100,
-      sortedWaste: 30,
-      unsortedWaste: 30,
-      alertLevel: 1
-    },
-    {
-      id: '3',
-      lat: 40.3500,
-      long: 18.1700,
-      capacity: 100,
-      sortedWaste: 70,
-      unsortedWaste: 20,
-      alertLevel: 2
-    },
-    {
-      id: '4',
-      lat: 40.3550,
-      long: 18.1720,
-      capacity: 100,
-      sortedWaste: 50,
-      unsortedWaste: 40,
-      alertLevel: 1
-    },
-    {
-      id: '5',
-      lat: 40.3580,
-      long: 18.1770,
-      capacity: 100,
-      sortedWaste: 80,
-      unsortedWaste: 10,
-      alertLevel: 0
-    },
-    {
-      id: '6',
-      lat: 40.3520,
-      long: 18.1850,
-      capacity: 100,
-      sortedWaste: 20,
-      unsortedWaste: 80,
-      alertLevel: 2
-    },
-    {
-      id: '7',
-      lat: 40.3450,
-      long: 18.1600,
-      capacity: 100,
-      sortedWaste: 60,
-      unsortedWaste: 30,
-      alertLevel: 2
-    },
-    {
-      id: '8',
-      lat: 40.3480,
-      long: 18.1760,
-      capacity: 100,
-      sortedWaste: 10,
-      unsortedWaste: 90,
-      alertLevel: 2
-    },
-    {
-      id: '9',
-      lat: 40.3590,
-      long: 18.1630,
-      capacity: 100,
-      sortedWaste: 70,
-      unsortedWaste: 20,
-      alertLevel: 2
-    },
-    {
-      id: '10',
-      lat: 40.3610,
-      long: 18.1720,
-      capacity: 100,
-      sortedWaste: 40,
-      unsortedWaste: 30,
-      alertLevel: 1
-    },
-    {
-      id: '11',
-      lat: 40.3570,
-      long: 18.1750,
-      capacity: 100,
-      sortedWaste: 10,
-      unsortedWaste: 60,
-      alertLevel: 1
-    },
-    {
-      id: '12',
-      lat: 40.3505,
-      long: 18.1780,
-      capacity: 100,
-      sortedWaste: 80,
-      unsortedWaste: 10,
-      alertLevel: 0
-    },
-    {
-      id: '13',
-      lat: 40.3557,
-      long: 18.1680,
-      capacity: 100,
-      sortedWaste: 20,
-      unsortedWaste: 70,
-      alertLevel: 1
-    },
-    {
-      id: '14',
-      lat: 40.3602,
-      long: 18.1820,
-      capacity: 100,
-      sortedWaste: 60,
-      unsortedWaste: 40,
-      alertLevel: 2
-    },
-    {
-      id: '15',
-      lat: 40.3543,
-      long: 18.1750,
-      capacity: 100,
-      sortedWaste: 30,
-      unsortedWaste: 30,
-      alertLevel: 1
-    },
-  ];
-   
+  baseUrl: string = "http://localhost:8081/api/bin/";
 
   constructor(private authService: AuthService, private httpClient: HttpClient) {
     this.authService.idTokenClaims$.subscribe(
@@ -166,29 +28,85 @@ export class BinService {
     })
   }
 
-  createBin(){
+  createBin(bin: Bin): Observable<any>{
+    const createBinURL = this.baseUrl + 'create';
 
+    if (this.authToken) {
+      this.httpOptions.headers = this.httpOptions.headers.set('Authorization', 'Bearer ' + this.authToken);
+    }
+    
+    return this.httpClient.post(createBinURL, bin, this.httpOptions);
   }
 
-  deleteBin(){
+  deleteBin(binID: string): Observable<any>{
+    const deleteBinURL = this.baseUrl + 'delete/' + binID;
 
+    if (this.authToken) {
+      this.httpOptions.headers = this.httpOptions.headers.set('Authorization', 'Bearer ' + this.authToken);
+    }
+    
+    return this.httpClient.delete(deleteBinURL, this.httpOptions);  
   }
 
-  getBin(){
+  getBin(binID: string): Observable<Bin>{
+    const getBinURL = this.baseUrl + 'get/' + binID;
 
+    if (this.authToken) {
+      this.httpOptions.headers = this.httpOptions.headers.set('Authorization', 'Bearer ' + this.authToken);
+    }
+    
+    return this.httpClient.get<Bin>(getBinURL, this.httpOptions).pipe(
+      catchError((error: any) => {
+        console.error('Error retrieving bin:', error);
+        throw error;
+      })
+    );  
   }
 
-  getAllBins(){
-    return this.bins;
+  getAllBins(): Observable<Bin[]> {
+    const getAllBinsURL = this.baseUrl + 'getAll';
+  
+    if (this.authToken) {
+      this.httpOptions.headers = this.httpOptions.headers.set('Authorization', 'Bearer ' + this.authToken);
+    }
+  
+    return this.httpClient.get<Bin[]>(getAllBinsURL, this.httpOptions).pipe(
+      catchError((error: any) => {
+        console.error('Error retrieving bins:', error);
+        throw error;
+      })
+    );
   }
+  
 
-  unloadBin(){
-
+  unloadBin(binID: string): Observable<any> {
+    const unloadBinURL = this.baseUrl + 'unload/' + binID;
+  
+    if (this.authToken) {
+      this.httpOptions.headers = this.httpOptions.headers.set('Authorization', 'Bearer ' + this.authToken);
+    }
+  
+    return this.httpClient.post(unloadBinURL, null, this.httpOptions).pipe(
+      catchError((error: any) => {
+        console.error('Error unloading bin:', error);
+        throw error;
+      })
+    );
   }
-
-  loadBin(){
-
+  
+  loadBin(binID: string, sortedWaste: number, unsortedWaste: number): Observable<any> {
+    const loadBinURL = this.baseUrl + 'setWaste/' + binID + '/' + sortedWaste + '/' + unsortedWaste;
+  
+    if (this.authToken) {
+      this.httpOptions.headers = this.httpOptions.headers.set('Authorization', 'Bearer ' + this.authToken);
+    }
+  
+    return this.httpClient.post(loadBinURL, null, this.httpOptions).pipe(
+      catchError((error: any) => {
+        console.error('Error loading bin:', error);
+        throw error;
+      })
+    );
   }
-
-
+  
 }
