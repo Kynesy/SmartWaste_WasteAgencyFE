893fd9c7732152b8bf587297aad3b170a22af906
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-28 13:53:26 +0200

    creato servizio alert e model

---
+++ src/app/models/alert.ts
@@ -0,0 +1,6 @@
+export interface Alert {
+    id: string,
+    timestamp: string,
+    binId: string
+    alertLevel: string
+}
---
+++ src/app/services/alerts.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { Observable, catchError } from 'rxjs';
+import { Alert } from '../models/alert';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertsService {
+  authToken: string | null = null
+  baseUrl: string = "http://localhost:8081/api/alert/";
+
+  constructor(private authService: AuthService, private httpClient: HttpClient) {
+    this.authService.idTokenClaims$.subscribe(
+      (token) => {
+        if(token && token['__raw']){
+          this.authToken = token['__raw']
+        }
+      }
+    )
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':'application/json',
+      'Authorization':''
+    })
+  }
+
+  getAllAlerts(): Observable<Alert[]> {
+    const getAllAlertsURL = this.baseUrl + 'getAll';
+  
+    if (this.authToken) {
+      this.httpOptions.headers = this.httpOptions.headers.set('Authorization', 'Bearer ' + this.authToken);
+    }
+  
+    return this.httpClient.get<Alert[]>(getAllAlertsURL, this.httpOptions).pipe(
+      catchError((error: any) => {
+        console.error('Error retrieving bins:', error);
+        throw error;
+      })
+    );
+  }
+
+}
