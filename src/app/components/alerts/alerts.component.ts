82a461a37fa25ab59ab8a8c9994143bbbfc8240c
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-28 13:53:54 +0200

    implementato componente alert
    - visualizza tutti gli alert usando il virtual scrolling

--- src/app/components/alerts/alerts.component.ts
+++ src/app/components/alerts/alerts.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Alert } from 'src/app/models/alert';
+import { AlertsService } from 'src/app/services/alerts.service';
 
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.component.html',
   styleUrls: ['./alerts.component.scss']
 })
-export class AlertsComponent {
+export class AlertsComponent implements OnInit{
+  alertList: Alert[] = [];
+  
+  constructor(private alertsService: AlertsService, private datePipe: DatePipe){}
 
+  ngOnInit(): void {
+      this.alertsService.getAllAlerts().subscribe(
+        (alerts: Alert[]) => {
+          this.alertList = alerts.reverse();
+        },
+        (error: any) => {
+          console.error('Error retrieving alerts:', error);
+        }
+      )
+  }
+
+  formatTimestamp(timestamp: string, format: string): string {
+    const date = new Date(timestamp);
+    return this.datePipe.transform(date, format) || '';
+  }
+  
 }
