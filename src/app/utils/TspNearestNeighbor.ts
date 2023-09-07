4289f6af052eb8466a955861d7db2ad5717ac531
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-09-07 17:32:13 +0200

    implementazioni calcolo routing sulla matrice di costi

---
+++ src/app/utils/TspNearestNeighbor.ts
@@ -0,0 +1,35 @@
+export default class TspNearestNeighbor {
+  findOptimalSolution(distances: number[][]): any {
+    const numLocations = distances.length;
+    const visitedNodes: number[] = [];
+    let totalDistance = 0;
+
+    // Start from the first location (node 0)
+    let currentNode = 0;
+    visitedNodes.push(currentNode);
+
+    while (visitedNodes.length < numLocations) {
+      let nearestNode = -1;
+      let minDistance = Number.MAX_VALUE;
+
+      // Find the nearest unvisited node
+      for (let nextNode = 0; nextNode < numLocations; nextNode++) {
+        if (!visitedNodes.includes(nextNode) && distances[currentNode][nextNode] < minDistance) {
+          nearestNode = nextNode;
+          minDistance = distances[currentNode][nextNode];
+        }
+      }
+
+      if (nearestNode !== -1) {
+        visitedNodes.push(nearestNode);
+        totalDistance += minDistance;
+        currentNode = nearestNode;
+      }
+
+    }
+
+    // aggiungi tempo per svuotare ogni cestino
+    totalDistance = totalDistance + distances.length * 160;
+    return { path: visitedNodes, distance: totalDistance };
+  }
+}
